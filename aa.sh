c4a7d4a2d260c72aaf30ede586e4d9d1755fdb01
capture0x <61238348+capture0x@users.noreply.github.com>
2021-01-18 15:49:20 +0000

    upload

---
+++ aa.sh
@@ -0,0 +1,7 @@
+wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chown root:root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
+
+
