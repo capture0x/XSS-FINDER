f24a501d4a8805f105780f044f634bd1efdc8319
capture0x <61238348+capture0x@users.noreply.github.com>
2023-05-24 08:55:57 -0400

    Update aa.sh

--- aa.sh
+++ aa.sh
@@ -1,4 +1,4 @@
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/113.0.5672.63/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chown root:root /usr/bin/chromedriver
