fc22559de4d46df4fd160718818512a9457d9600
capture0x <61238348+capture0x@users.noreply.github.com>
2023-08-04 16:49:58 -0400

    Update aa.sh

--- aa.sh
+++ aa.sh
@@ -1,4 +1,4 @@
-curl -O https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/115.0.5790.170/linux64/chrome-linux64.zip
+curl -O https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/115.0.5790.170/linux64/chromedriver-linux64.zip
 unzip chromedriver-linux64.zip
 cd chromedriver-linux64
 sudo mv chromedriver /usr/bin/chromedriver
